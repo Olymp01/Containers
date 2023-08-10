69fff4e1a0ccf5d8385d0e264419ad87353af89f
Olymp01 <141898960+Olymp01@users.noreply.github.com>
2023-08-10 14:49:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -21,9 +21,7 @@ def run_query(query):
     rows = conn.execute(query, headers=1)
     rows = rows.fetchall()
     return rows
-
 sheet_url = st.secrets["private_gsheets_url"]
-workbook = load_workbook(sheet_url, data_only=True)
-second_sheet = workbook.worksheets[1]
-cell_value = second_sheet['A1'].value
-st.write(cell_value)
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+df = pd.DataFrame(rows)
+st.write(rows)
