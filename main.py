5f9a51eb516d8d357771bbb14d5be431596b772e
Olymp01 <141898960+Olymp01@users.noreply.github.com>
2023-08-10 15:12:48 +0600

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ log_name = st.text_input('Имя')
 log_title = st.text_input('Логин')
 log_pass = st.text_input('Пароль') 
 
+options = {'Пробирка со средой Кэри Блера':'Z01','Пробирка со средой Эймса':'Z02', 'Уреазный тест(выдыхаемый воздух)':'Z03', 'Пробирка с желтой крышкой (ЦФДА)':'Z04',
+          'Зеленая крышка без геля':'Z05','Конверт с ватной палочкой':'Z06','Баночка для кала':'Z07','Стерильный контейнер для мочи':'Z08',
+           'Нестерильный контейнер для мочи':'Z09','Контейнер для мочи с иглой':'Z10','Контейнер для слюны':'Z11','Контейнер для спермы':'Z12',
+           'Коричневая пробирка для мочи':'Z13','Мазок на степень чистоты':'Z14','Мазок отд. уретры':'Z15','РИФ':'Z16','Соскоб Эппендорф векторбест':'Z17',
+           'Соскоб Эппендорф амплисенс':'Z18','Соскоб (цитология ПАП и шейки матки)':'Z19','Срез':'Z20','Стерильный контейнер для бакпосева':'Z21',
+           'Тест полоска(тест антиген)':'Z22','Квантиферон 4 вакутейнера':'Z23','Перианальный соскоб(ректально)':'Z28','Пробирка "Streck" (trisomnia)':'Z29',
+           'Соскоб ПЦР на коронавирус':'Z30','Микоплаза':'Z31','Фильтр-карта':'Z32'
+          }
+
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=[
@@ -27,6 +36,7 @@ def run_query(query):
     rows = conn.execute(query, headers=1)
     rows = rows.fetchall()
     return rows
+    
 sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 df = pd.DataFrame(rows)
@@ -35,5 +45,19 @@ st.write(df)
 def auth(sheet_url,new_gid):
     return sheet_url.replace("gid=0","gid="+new_gid)
     
-
+for i in range(len(xd['Login'])):
+    if log_title == xd['Login'][i] and log_pass == xd['Password'][i]:
+        needed_sheet = auth(xd['Worksheet'][i])
+        rows = run_query(f'SELECT * FROM "{sheet_url}"')
+        df2 = pd.DataFrame(rows)
+        st.write(df2)
+        token = xd['Token'][i]
+        st.write('Префикс: ',token)
+        option = st.selectbox('Выбрать контейнер', options.keys())
+        cont_prefix = options.get(option)   
+        st.write(option,':',cont_prefix)
+        number = st.number_input('Введите количество пробирок', min_value=0, step=1)
+        st.write(number)
+        
+        coeff = 4
 
