37dc5f090fa067b1fe38f46d8d2b632ae70b062c
Olymp01 <141898960+Olymp01@users.noreply.github.com>
2023-08-11 10:15:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ client = gspread.authorize(credentials)
 # Perform SQL query on the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
-def load_data(url, sheet_name="Astana"):
+def load_data(url, sheet_name="Autorization"):
     sh = client.open_by_url(url)
     df = pd.DataFrame(sh.worksheet(sheet_name).get_all_records())
     return df
